af4d455e689a502d6b6bc5227b2725980e7e9f92
18211a1239 <78413431+18211a1239@users.noreply.github.com>
2022-08-19 22:20:22 +0530

    Update ManagerController.java

--- src/main/java/cts/mfpe/manager/controllers/ManagerController.java
+++ src/main/java/cts/mfpe/manager/controllers/ManagerController.java
@@ -25,7 +25,7 @@ import cts.mfpe.manager.exceptions.ExecutiveNotFoundException;
 import cts.mfpe.manager.services.ManagerService;
 
 @RestController
-@CrossOrigin("http://real-state-project2.s3-website-us-west-1.amazonaws.com")
+@CrossOrigin
 public class ManagerController {
 
 	@Autowired
